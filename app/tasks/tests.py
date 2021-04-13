8be7e3725e9182a9c056bc57e7d528eb32575aa9
Mani Shirvani <m.shirvani@live.com>
2021-04-13 16:49:13 +0100

    sample tests

--- app/tasks/tests.py
+++ app/tasks/tests.py
@@ -1,5 +1,13 @@
-from django.test import TestCase
+import pytest
+
+pytestmark = pytest.mark.django_db
 
 
 def test_app():
     assert True
+
+
+def test_home(client):
+    response = client.get('')
+    assert response.status_code == 200
+    assert 'home.html' in response.template_name
--- app/tasks/urls.py
+++ app/tasks/urls.py
@@ -1,13 +1,12 @@
 from ariadne.contrib.django.views import GraphQLView
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 from tasks.views import TasksListView
-
 from tasks.resolver import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TasksListView.as_view()),
+    path('', TasksListView.as_view(), name='home'),
     path('graphql/', GraphQLView.as_view(schema=schema), name='graphql'),
 ]
